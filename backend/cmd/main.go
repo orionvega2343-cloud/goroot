fc8f42da9140409b80346b90fac65a9cffcb7200
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-03 18:05:04 +0300

    feat(main): wire up dependency graph and start HTTP server

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"backend/internal/config"
+	"backend/internal/db"
+	"backend/internal/handler"
+	"backend/internal/notifier"
+	"backend/internal/repository"
+	"backend/internal/service"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	//Загрузка конфига
+	cfg := config.MustLoad()
+
+	//Покдлючение БД
+	database, err := db.ConnDB(*cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	pref := tele.Settings{
+		Token: os.Getenv("BOT_TOKEN"),
+	}
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	not := notifier.NewNotifier(b)
+	repo := repository.NewApplicationRepo(database)
+	svc := service.NewApplicationService(repo, not)
+	hndlr := handler.NewApplicationHandler(svc)
+
+	//Роутер
+	r := gin.New()
+
+	r.POST("/application", hndlr.PostApplication)
+	r.Run(":8080")
+}
