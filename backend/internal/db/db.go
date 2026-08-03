b02de5b74fc1a39dd9fc495f3ccdc80ea6570dc3
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-03 15:32:09 +0300

    feat: add connect database

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -1,9 +1,19 @@
 package db
 
 import (
+	"backend/internal/config"
+	"database/sql"
+	"fmt"
+
 	_ "github.com/lib/pq"
 )
 
-//func ConnDB() (*sql.DB, error) {
-//	connStr := fmt.Sprintf("name: %s, host, %s, port: %d, username: %s, password: %s, ssl_mode: %s")
-//}
+func ConnDB(cfg config.Config) (*sql.DB, error) {
+	connStr := fmt.Sprintf("name: %s, host, %s, port: %d, username: %s, password: %s, ssl_mode: %s", cfg.DB.Name, cfg.DB.Host, cfg.DB.Port, cfg.DB.User, cfg.DB.Password, cfg.DB.SslMode)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
