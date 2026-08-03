00bbcdc5f90e03922a276001115ecffac413f7d2
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-03 15:22:29 +0300

    feat: add config for DB connect

---
+++ backend/internal/db/db.go
@@ -0,0 +1,9 @@
+package db
+
+import (
+	_ "github.com/lib/pq"
+)
+
+//func ConnDB() (*sql.DB, error) {
+//	connStr := fmt.Sprintf("name: %s, host, %s, port: %d, username: %s, password: %s, ssl_mode: %s")
+//}
